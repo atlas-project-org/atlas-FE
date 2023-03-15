32f8356d85ebc36f003daa99f887939bba8ccd46
YJK-7 <2019yjk@gmail.com>
2023-03-15 15:01:42 +0900

    apply sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,16 +11,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <h1 className="header">Welcome to the &lt;insert game name here&gt;</h1>
+      <main className="page">
+        <div className="header-container">
+          <h1 className="header">Welcome to the <br/> &lt;insert game name here&gt;</h1>
         </div>
 
         <div className="button-container">
-          <button className="start-btn">Start your adventure!</button>
+          <button className="start-btn">Start <br/> Adventure</button>
         </div>
-
-        
       </main>
     </>
   )
